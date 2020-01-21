9de4fefff95b733150a0b4541604350acb16706f
kamyranwilliams <37843545+kamyranwilliams@users.noreply.github.com>
2020-01-21 14:42:42 -0800

    Add files via upload

---
+++ tetris.rb
@@ -0,0 +1,13 @@
+# Welcome to Sonic Pi v3.1
+#Tetris
+use_bpm 140
+#https://www.youtube.com/watch?v=K_E5C9CdxyI/
+#https://github.com/nickarora/tetris/tree/master/sounds
+#https://projects.raspberrypi.org/en/projects/tetris-theme
+#https://virtualpiano.net/?s=tetris
+#https://sonic-pi.mehackit.org/exercises/en/05-advanced-topics-1/04-external-samples.html
+#https://gist.github.com/darinwilson/137764d4b06072de626c/
+use_synth :piano
+play_pattern
+
+/e3/
